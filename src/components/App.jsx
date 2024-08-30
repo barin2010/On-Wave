ce072f458eb24ec85680651607ff8cd657b64782
barin2010 <uyrirudenko@gmail.com>
2024-08-31 02:14:25 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,28 @@
+import React, { Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Catalog from './pages/Catalog';
-import Contacts from './pages/Contacts';
-import Home from './pages/Home';
-import News from './pages/News';
 import Navigation from './Navigation/Navigation';
 import Footer from './Footer/Footer';
+
+// Lazy load the pages
+const Home = React.lazy(() => import('./pages/Home'));
+const Catalog = React.lazy(() => import('./pages/Catalog'));
+const News = React.lazy(() => import('./pages/News'));
+const Contacts = React.lazy(() => import('./pages/Contacts'));
 
 export const App = () => {
   return (
     <div>
       <Navigation />
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/catalog" element={<Catalog />} />
-        <Route path="/news" element={<News />} />
-        <Route path="/contacts" element={<Contacts />} />
-      </Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/catalog" element={<Catalog />} />
+          <Route path="/news" element={<News />} />
+          <Route path="/contacts" element={<Contacts />} />
+        </Routes>
+      </Suspense>
+
       <Footer />
     </div>
   );

41d0b0d6f2aed4a4f805041c6102636ad87c01ac
barin2010 <uyrirudenko@gmail.com>
2024-08-31 03:21:08 +0300

    fixed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,6 @@ import { Routes, Route } from 'react-router-dom';
 import Navigation from './Navigation/Navigation';
 import Footer from './Footer/Footer';
 
-// Lazy load the pages
 const Home = React.lazy(() => import('./pages/Home'));
 const Catalog = React.lazy(() => import('./pages/Catalog'));
 const News = React.lazy(() => import('./pages/News'));

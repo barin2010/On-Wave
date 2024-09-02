fc745c6a8193c19a922e33e5beb8f2fcfeb5e4bb
barin2010 <39887811+barin2010@users.noreply.github.com>
2024-09-02 16:43:42 +0000

    Deploying to gh-pages from @ barin2010/On-Wave@4ca8aba7eab64838b5a37025a7c9f87656c36e17 🚀

--- static/js/927.04710ca2.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[927],{7927:function(e,t,a){a.r(t);a(2791);var r=a(184);t.default=function(){return(0,r.jsx)("div",{children:"Catalog"})}}}]);
-//# sourceMappingURL=927.04710ca2.chunk.js.map
